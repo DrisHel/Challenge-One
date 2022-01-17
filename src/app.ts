1e404b702efdd9ef2c6b62b2cfe1e0fe572056d0
Doris Nunes <doris90nunes@gmail.com>
2022-01-17 20:05:04 -0300

    feat(swagger)added route.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database/index';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 import { routes } from './app/routes/index';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export { app };
