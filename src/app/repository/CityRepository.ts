3acab259982c433aca75a5cad57669eb8a8675ab
Doris Nunes <doris90nunes@gmail.com>
2022-01-18 14:39:49 -0300

    refactor(getConnection).

--- src/app/repository/CityRepository.ts
+++ src/app/repository/CityRepository.ts
@@ -1,9 +1,9 @@
-import { getRepository } from 'typeorm';
+import { getConnection } from 'typeorm';
 import { City } from '../entities/City';
 
 class CityRepository {
   async create(payload) {
-    const repo = getRepository(City);
+    const repo = getConnection(process.env.NODE_ENV).getRepository(City);
     const { city, state } = payload;
     const cities = repo.create({ city, state });
     await repo.save(cities);
@@ -11,7 +11,7 @@ class CityRepository {
   }
 
   async find(payload): Promise<[City[], number]> {
-    const repo = getRepository(City);
+    const repo = getConnection(process.env.NODE_ENV).getRepository(City);
 
     const cities = await repo.findAndCount(payload);
 
@@ -19,31 +19,31 @@ class CityRepository {
   }
 
   async delete(id) {
-    const repo = getRepository(City);
+    const repo = getConnection(process.env.NODE_ENV).getRepository(City);
     const result = await repo.delete(id);
     return result;
   }
 
   async update(id, payload) {
-    const repo = getRepository(City);
+    const repo = getConnection(process.env.NODE_ENV).getRepository(City);
     const result = await repo.update(id, payload);
     return result;
   }
 
   async findOne(id): Promise<City | Error> {
-    const repo = getRepository(City);
+    const repo = getConnection(process.env.NODE_ENV).getRepository(City);
     const result = await repo.findOne(id);
     return result;
   }
 
   async findCity(city): Promise<City | Error> {
-    const repo = getRepository(City);
+    const repo = getConnection(process.env.NODE_ENV).getRepository(City);
     const result = await repo.findOne(city);
     return result;
   }
 
   async findState(state): Promise<City | Error> {
-    const repo = getRepository(City);
+    const repo = getConnection(process.env.NODE_ENV).getRepository(City);
     const result = await repo.findOne(state);
     return result;
   }
