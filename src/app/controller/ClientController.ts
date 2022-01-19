c03e24d1a19a42e30f3a4e7356b286ce8c9a2b71
Doris Nunes <doris90nunes@gmail.com>
2022-01-19 14:42:38 -0300

    refactor ( remove code) Removed useless code.

--- src/app/controller/ClientController.ts
+++ src/app/controller/ClientController.ts
@@ -50,15 +50,6 @@ class ClientController {
       return response.status(400).json(error);
     }
   }
-
-  async getName(request: Request, response: Response) {
-    try {
-      const result = await clientService.findN(request.params);
-      return response.status(200).json(result);
-    } catch (error) {
-      return response.status(400).json(error);
-    }
-  }
 }
 
 export { ClientController };
