173ec0db759d5d11df432034183a4cac96d3b0bb
Doris Nunes <doris90nunes@gmail.com>
2022-01-21 17:38:56 -0300

    refactor ( renamed folder )renamed folder to adapt files referring to the city.

---
+++ __tests__/TestCity/CreateCity.test.ts
@@ -0,0 +1,76 @@
+import request from 'supertest';
+import { app } from '../../src/app';
+
+describe('create client', () => {
+  it('returns status 201', async () => {
+    const cityMock = {
+      city: 'Porto Alegre',
+
+      state: 'RS'
+    };
+
+    const response = await request(app).post('/city').send(cityMock);
+
+    expect(response.statusCode).toEqual(201);
+
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('returns bad request ', async () => {
+    const cityMock = {
+      city: 'Pelotas',
+
+      state: 'rs'
+    };
+
+    const res = await request(app).post('/city').send(cityMock);
+
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it('Bad request ', async () => {
+    const city = {
+      city: ' ',
+
+      state: 'rs '
+    };
+
+    const res = await request(app).post('/city').send(city);
+
+    expect(res.statusCode).toEqual(400);
+  });
+  it('Bad request ', async () => {
+    const cityMock = {
+      city: 'Rio Grande',
+
+      state: ' '
+    };
+
+    const res = await request(app).post('/city').send(cityMock);
+
+    expect(res.statusCode).toEqual(400);
+  });
+  it('Bad request ', async () => {
+    const cityMock = {
+      city: 102030,
+
+      state: 10
+    };
+
+    const res = await request(app).post('/city').send(cityMock);
+
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it('Bad request ', async () => {
+    const cityMock = {
+      city: 'Gramado',
+
+      state: 'Rs'
+    };
+
+    const res = await request(app).post('/city').send(cityMock);
+
+    expect(res.statusCode).toEqual(400);
+  });
+});
