c227acafdeb5d4e1af96909e6a445f32842df6aa
Doris Nunes <doris90nunes@gmail.com>
2022-01-21 17:25:32 -0300

    test - (adding missing tests)created file for existing registration error.

---
+++ src/app/errors/AlreadyExists.ts
@@ -0,0 +1,11 @@
+export class AlreadyExists extends Error {
+  description: string;
+
+  constructor() {
+    super();
+
+    this.description = 'Bad request';
+
+    this.message = `Already existing information.`;
+  }
+}
