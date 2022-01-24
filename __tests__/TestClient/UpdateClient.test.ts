3abe2ba1a4f96da8f6f6e6fc6d8ae1e1bce8bcf5
Doris Nunes <doris90nunes@gmail.com>
2022-01-24 18:01:11 -0300

    test (test creation for client )creation of test file for client, update and find by id.

---
+++ __tests__/TestClient/UpdateClient.test.ts
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { app } from '../../src/app';
+
+describe('create client', () => {
+  it('returns status 201', async () => {
+    const cityMock = {
+      city: 'Passo Fundo',
+
+      state: 'RS'
+    };
+
+    const resCity = await request(app).post('/city').send(cityMock);
+    const clientMock = {
+      name: 'Ana Maria Braga ',
+      gender: 'F',
+      birthdate: '1980/02/14',
+      cityId: `${resCity.body.id}`
+    };
+    const response = await request(app).post('/client').send(clientMock);
+    const response1 = await request(app).get(`/client/${response.body.id}`);
+    expect(response1.body).toHaveProperty('id');
+    const upClient = {
+      name: 'Pedro Cosby ',
+      gender: 'M',
+      birthdate: '1980/02/14',
+      cityId: `${resCity.body.id}`
+    };
+    const response2 = await request(app).put(`/client/${response.body.id}`).send(upClient);
+
+    expect(response2.statusCode).toEqual(200);
+  });
+});
--- __tests__/TestClient/findByIdClient.test.ts
+++ __tests__/TestClient/findByIdClient.test.ts
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import { app } from '../../src/app';
 
-describe('Remove client!', () => {
-  it('returns status 204', async () => {
+describe('create client', () => {
+  it('returns status 201', async () => {
     const cityMock = {
       city: 'Passo Fundo',
 
@@ -16,9 +16,9 @@ describe('Remove client!', () => {
       birthdate: '1980/02/14',
       cityId: `${resCity.body.id}`
     };
-    const response1 = await request(app).post('/client').send(clientMock);
-    const response2 = await request(app).delete(`/client/${response1.body.id}`);
+    const response = await request(app).post('/client').send(clientMock);
+    const response1 = await request(app).get(`/client/${response.body.id}`);
 
-    expect(response2.statusCode).toEqual(204);
+    expect(response1.statusCode).toEqual(200);
   });
 });
