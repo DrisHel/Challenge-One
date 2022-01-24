80966cbad608173e27a42558b7488029fc053859
Doris Nunes <doris90nunes@gmail.com>
2022-01-24 10:02:29 -0300

    fix (externalize) externalized gender and imported the enumcity field in validation.

--- src/app/validation/client/createValidationClient.ts.ts
+++ src/app/validation/client/createValidationClient.ts.ts
@@ -1,10 +1,11 @@
 import Joi from 'joi';
+import { enumCity } from '../../utils/enumCity';
 
 export = async (req, res, next) => {
   try {
     const Client = Joi.object({
       name: Joi.string().min(10).max(40).required(),
-      gender: Joi.string().trim().valid('F', 'M', 'Other').required(),
+      gender: Joi.string().trim().valid(enumCity).required(),
       birthdate: Joi.date().required(),
       age: Joi.number(),
       cityId: Joi.string().uuid().required()
