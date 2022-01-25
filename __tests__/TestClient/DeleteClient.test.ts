7d6acb9c700aed4d3b3959fe4b39c236cf5d2cba
Doris Nunes <doris90nunes@gmail.com>
2022-01-25 09:23:11 -0300

    test ( created test delete) added test for client delete field.

--- __tests__/TestClient/DeleteClient.test.ts
+++ __tests__/TestClient/DeleteClient.test.ts
@@ -21,4 +21,25 @@ describe('Remove client!', () => {
 
     expect(response2.statusCode).toEqual(204);
   });
+  it('returns status 400', async () => {
+    const cityMock = {
+      city: 'Passo Fundo',
+
+      state: 'RS'
+    };
+
+    const resCity = await request(app).get('/city').send(cityMock);
+    const iderrado = 12;
+    const clientMock = {
+      name: 'Ana Maria Braga ',
+      gender: 'F',
+      birthdate: '2000/01/01',
+      cityId: `${resCity.body.id}`
+    };
+    const response = await request(app).get(`/client/${iderrado}`).send(clientMock);
+
+    const { status } = response;
+
+    expect(status).toEqual(400);
+  });
 });
