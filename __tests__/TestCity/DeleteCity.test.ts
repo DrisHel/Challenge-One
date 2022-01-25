9421968253b830d5aa649bc785d7ee4db108e038
Doris Nunes <doris90nunes@gmail.com>
2022-01-25 09:03:58 -0300

    test ( test delete) added test for city delete field.

--- __tests__/TestCity/DeleteCity.test.ts
+++ __tests__/TestCity/DeleteCity.test.ts
@@ -14,4 +14,16 @@ describe('Remove city', () => {
 
     expect(response2.statusCode).toEqual(204);
   });
+
+  it('Not Found ', async () => {
+    const resp = await request(app).delete('/city/04e966c9-88e0-442b-9802-397ca310d134');
+
+    expect(resp.body).toEqual({
+      description: 'Not found',
+
+      message: 'The ID: [object Object] was not found',
+
+      statusCode: 404
+    });
+  });
 });
