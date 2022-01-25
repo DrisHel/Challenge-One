28f2ba2ed7f9c6800b4c0465e332dba83b99a7fc
Doris Nunes <doris90nunes@gmail.com>
2022-01-25 09:21:09 -0300

    test ( created test update) added test for city update field.

--- __tests__/TestClient/UpdateClient.test.ts
+++ __tests__/TestClient/UpdateClient.test.ts
@@ -29,4 +29,41 @@ describe('create client', () => {
 
     expect(response2.statusCode).toEqual(200);
   });
+  it('returns status bad request', async () => {
+    const cityMock = {
+      city: 'Porto Alegre',
+
+      state: 'RS'
+    };
+
+    const resCity = await request(app).post('/city/').send(cityMock);
+
+    const clientMock = {
+      name: 'Barbara',
+
+      gender: 'F',
+
+      city_home: `${resCity.body.id}`,
+
+      birthdate: '11/10/1995'
+    };
+
+    const create = await request(app).post('/client/').send(clientMock);
+
+    await request(app).get(`/client/${create.body.id}`);
+
+    const clientUPMock = {
+      name: 'Tuco',
+
+      gender: 'M',
+
+      city_home: `${resCity.body.id}`
+    };
+    const response = await request(app)
+      .put(`/client/${'4a271b3e-2c2e-477f-ab58-4a5ebc35dec1'}`)
+
+      .send(clientUPMock);
+
+    expect(response.body).toEqual({ message: 'incorrect id' });
+  });
 });
