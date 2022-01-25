fbbad0d8d3d54df1b979f015d1b45f60a7e75b48
Doris Nunes <doris90nunes@gmail.com>
2022-01-25 09:22:15 -0300

    test ( created test fidAll) added test for client findAll field.

--- __tests__/TestClient/FindAllClient.test.ts
+++ __tests__/TestClient/FindAllClient.test.ts
@@ -22,4 +22,24 @@ describe('Find the customer!', () => {
 
     expect(status).toEqual(200);
   });
+  it('returns status 200', async () => {
+    const cityMock = {
+      city: 'Passo Fundo',
+
+      state: 'RS'
+    };
+
+    const resCity = await request(app).get('/city').send(cityMock);
+    const clientMock = {
+      name: 'Ana Maria Braga ',
+      gender: 'F',
+      birthdate: '2000/01/01',
+      cityId: `${resCity.body.id}`
+    };
+    const response = await request(app).get('/client/?birthdate=2000/01/01').send(clientMock);
+
+    const { status } = response;
+
+    expect(status).toEqual(200);
+  });
 });
