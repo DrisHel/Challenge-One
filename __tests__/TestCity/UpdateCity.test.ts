7c6cbe4e75eff48609f661d7ee04d14250c002fd
Doris Nunes <doris90nunes@gmail.com>
2022-01-25 09:26:08 -0300

    test ( created test update) added test for city update field.

--- __tests__/TestCity/UpdateCity.test.ts
+++ __tests__/TestCity/UpdateCity.test.ts
@@ -20,4 +20,22 @@ describe('Update city', () => {
 
     expect(response2.statusCode).toEqual(200);
   });
+  it('returns status 400 bad request', async () => {
+    const cityMock = {
+      city: 'Porto Alegre',
+      state: 'RS'
+    };
+    const create = await request(app).post('/city/').send(cityMock);
+
+    const res = await request(app).get(`/city/${create.body.id}`);
+    expect(res.body).toHaveProperty('id');
+
+    const cityUpMock = {
+      city: 'Porto Alegre',
+      state: 'RS'
+    };
+
+    const response = await request(app).put(`/city/${create.body.id}`).send(cityUpMock);
+    expect(response.statusCode).toEqual(400);
+  });
 });
